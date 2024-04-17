d836129dcdc27fedf9b567d3b5a24af50676b756
reosfire <2.com9761721@gmail.com>
2024-04-17 10:28:00 +0300

    docs for clamp function

--- common/src/main/kotlin/com/daylifecraft/common/util/extensions/Range.kt
+++ common/src/main/kotlin/com/daylifecraft/common/util/extensions/Range.kt
@@ -1,5 +1,8 @@
 package com.daylifecraft.common.util.extensions
 
+/**
+ * Clamps [value] to received range borders.
+ */
 fun <T : Comparable<T>> ClosedRange<T>.clamp(value: T): T = when {
   value > endInclusive -> endInclusive
   value < start -> start
